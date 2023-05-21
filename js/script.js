e6ff5be936a70928c7d06f25ed4dae611f917d96
jay <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-05-21 22:13:46 +0530

    update2.3.1

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Typing Script JS
 var typed = new Typed(".typing",{
-    strings: ["Web Designer", "Web Developer", "Freelancer", "Bounty Hunter","Frontend Developer"],
+    strings: ["Web Designer", "Web Developer", "Freelancer", "Bounty Hunter","Front-end Developer"],
     typeSpeed: 80,
     backSpeed: 80,
     loop: true,
 });
 
 var typed = new Typed(".typing-2",{
-    strings: ["Web Developer", "Web Designer", "Freelancer", "Bounty Hunter","Frontend Developer"],
+    strings: ["Web Developer", "Web Designer", "Freelancer", "Bounty Hunter","Front-end Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
