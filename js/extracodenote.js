a042b4620ddd2105128462ffcd579a0dc525815c
JBORAD988 <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-05-23 08:25:02 +0530

    update3.3.3

---
+++ js/extracodenote.js
@@ -0,0 +1,18 @@
+//change title
+
+//change title on focus
+// let alertShow = false;
+// let docTitle = document.title;
+// window.addEventListener("blur", () => {
+//     document.title = "Hey! Come back :( ";
+// });
+// window.addEventListener("focus", () => {
+//     // document.title = docTitle;
+//     setInterval(() => {
+//         document.title = alertShow ? "Hey I'm Jay" : 'Welcome! to my portfolio';
+//
+//         alertShow = !alertShow;
+//     }, 1000);
+//
+// });
+
