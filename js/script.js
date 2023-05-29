dbe161d7a23dad25a457041ec2f74b78eec6ecba
JBORAD988 <co-20-081@rctiahmedabad.gujgov.edu.in>
2023-05-29 23:02:58 +0530

    update4.1

--- js/script.js
+++ js/script.js
@@ -79,3 +79,22 @@ window.addEventListener('scroll', () => {
 
 
 
+// Animated Cursor
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+
+    cursor.setAttribute("style", "top: "+(e.pageY - 15)+"px; left: "+(e.pageX - 15)+"px;");
+
+
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
+
